6f96128a96be640905217ffc5d0b3c7bb2f66003
Paduka-Suryan <arsilid2@gmail.com>
2025-09-09 16:04:15 +0800

    Add files via upload

---
+++ (Day2) penggunaan println print dan prinf .java
@@ -0,0 +1,21 @@
+public class main {  
+    public static void main(String[] args) {
+        
+        //Menggunakan println
+        System.out.println("Nama: Muhammad Ade Arsyil Suryan");
+        System.out.println("Prodi: Informatika A.");
+        System.out.println("Umur: 19 tahun ");
+        System.out.println();
+        
+        //Menggunakan print
+        System.out.print("Nama: Muhammad Ade Arsyil Suryan");
+        System.out.print("Prodi Informatika A.");
+        System.out.print("Umur: 19 tahun");
+        System.out.println();
+        
+        //Menggunakan printf
+        System.out.printf("\nNama: %s %nUmur: %d %nProdi: %s ","Muhammad Ade Arsyil Suryan",19,"Informatika A.");
+        
+    }
+    
+}
