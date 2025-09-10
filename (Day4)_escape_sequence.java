af80e657a1efa9f419a73acd3e0e585a9c2519cc
Paduka-Suryan <arsilid2@gmail.com>
2025-09-11 01:20:10 +0800

    Add files via upload

---
+++ (Day4)_escape_sequence.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args){
+        System.out.println("Hello\nWorld"); // (\n) untuk membuat baris baru
+        System.out.println("---------------------");
+        System.out.println("Nama\t: Paduka"); // (\t) untuk menambahkan semacam tab
+        System.out.println("Prodi\t: Informatika");
+        System.out.println("---------------------");
+        System.out.println("Dia berkata: \"Halo!\""); // (\")untuk menambahkan tanda kutip dalam sebuah string
+    }
+}
