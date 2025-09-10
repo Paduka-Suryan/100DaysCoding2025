c7db80f3d8bd021f78b1bc33f3469262db219ff7
Paduka-Suryan <arsilid2@gmail.com>
2025-09-10 16:35:00 +0800

    Add files via upload

---
+++ (Day3)_memahami_dan_menggunkan_komentar.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		//komentar ini cocok untuk penjelasan singkat.
+		
+		/*Ini adalah komentar
+		yg bisa membuat
+		beberapa baris */
+		
+		/**ini adalah komentar untuk dokumentasi
+		Javadoc dapat memproses komentar ini untuk
+		menghasilkan dokumentasi HTML resmi dari kode anda,
+		yang sangat berguna bagi pengembang lain*/
+		
+		System.out.print("komentar tidak akan muncul pada output");
+		System.out.print("\n\noke, itu saja :v");
+	}
+}
