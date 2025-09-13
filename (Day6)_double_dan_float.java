929a7965c1c54a0f54a5c0ea4aca8916748a1901
Paduka-Suryan <arsilid2@gmail.com>
2025-09-13 13:14:33 +0800

    Add files via upload

---
+++ (Day6)_double_dan_float.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// Float menggunakan 32-bit dan memiliki presisi sekitar 7 digit desimal
+		float a = 5.5555555f;
+		
+		//sedangkan double menggunakan 64-bit (dua kali lipat) dan memberikan presisi lebih tinggi, sekitar 15 digit desimal
+		double b = 2.222222222222222;
+		
+		System.out.print("Float \t: "+a+"\nDouble \t: "+b);
+	}
+}
