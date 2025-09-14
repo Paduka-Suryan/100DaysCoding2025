8b9b324e1d034502a54214a5a43ebe174b0f8264
Paduka-Suryan <arsilid2@gmail.com>
2025-09-14 10:33:54 +0800

    Add files via upload

---
+++ (Day7)_char_dan_boolean.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		char a = 'A';
+		char b = 'B';
+		System.out.println("saya dari kelas "+a+" bukan dari kelas "+b);
+		System.out.print("\nkelas A lebih baik dari kelas B: ");
+		System.out.print(a<b);
+	}
+}
