df2a172bdc90e03e58fa104c5a7a4c4bcd9e6ec6
Paduka-Suryan <arsilid2@gmail.com>
2025-09-15 09:57:42 +0800

    Add files via upload

---
+++ (Day8)_string.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String a = "ini string";
+		System.out.println(a);
+		
+		//ini untuk menghitung jumah karakter pada string
+		System.out.println("jumah karakter pada teks di atas: "+a.length());
+	}
+}
