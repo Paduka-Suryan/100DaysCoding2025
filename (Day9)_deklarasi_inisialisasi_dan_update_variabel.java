d700c5d59c6f0358c4c8e1b5b45b5fd218b981d5
Paduka-Suryan <arsilid2@gmail.com>
2025-09-16 17:51:42 +0700

    Add files via upload

---
+++ (Day9)_deklarasi_inisialisasi_dan_update_variabel.java
@@ -0,0 +1,21 @@
+package paduka;
+public class main {
+    public static void main(String[] args) {
+        //ini deklarasi variabel
+        String nama ;
+        int usia ;
+        
+        //ini inisialisasi variabel
+        nama = "Paduka";
+        usia = 18;
+        
+        System.out.println("Nama: "+nama);
+        System.out.println("Usia tahun lalu: "+usia);
+        
+        //ini update variabel
+        usia = 19;
+        System.out.println("Usia sekarang :"+usia);
+        
+        //ini inisialisasi
+    }
+}
