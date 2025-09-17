982f68d4afd3ee2ca23e88d1bc8eb0641732303f
Paduka-Suryan <arsilid2@gmail.com>
2025-09-17 10:01:38 +0700

    Add files via upload

---
+++ (Day10)_konstanta.java
@@ -0,0 +1,17 @@
+package paduka;
+
+public class Paduka {
+    public static void main(String[] args) {
+        
+        //konsanta adalah variabel yang tidak bisa diubah setelah diberi nilai awal.
+        final String iniKonstanta = "contoh 1";
+        // iniKonstanta = "contoh"; ERROR! Tidak bisa diubah
+        
+        String iniBukanKonstanta = "contoh 2";
+        iniBukanKonstanta = "contoh 2 berhasil diubah";
+        
+        System.out.println(iniKonstanta+" tidak bisa dubah");
+        System.out.println(iniBukanKonstanta);
+    }
+    
+}
