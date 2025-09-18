9047c899e3711cb33d8e8a0294e1678a6a05570e
Paduka-Suryan <arsilid2@gmail.com>
2025-09-18 16:31:13 +0700

    Add files via upload

---
+++ (Day11)_scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class paduka {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("\nMasukkan Nama Anda: ");
+        String username = sc.nextLine();
+        sc.close();
+    }
+}
