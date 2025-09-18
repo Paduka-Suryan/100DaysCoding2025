3210f2b248898cfd50d75647800cbba2ff5134aa
Paduka-Suryan <arsilid2@gmail.com>
2025-09-18 23:43:22 +0700

    Add files via upload

---
+++ (Day12)_program_sederhana_bio_data.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		String a,b,c,d,e,f;
+		a="Paduka";
+		b="Informatika";
+		c="19 tahun";
+		d="Tutar";
+		System.out.println("Namaku\t:"+a);
+		System.out.println("Prodi\t:"+b);
+		System.out.println("Umur\t:"+c);
+		System.out.println("Alamat\t:"+d);
+	}
+}
