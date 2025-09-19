858b92edb190e70e23f2dcbd4d1b08fda5a55ce5
Paduka-Suryan <arsilid2@gmail.com>
2025-09-19 12:27:35 +0700

    Add files via upload

---
+++ Day12_bio_data.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Day12_bio_data {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== BIO DATA SISWA ===");
+
+        System.out.print("Masukkan Nama: ");
+        final String nama = input.nextLine();
+
+        System.out.print("Masukkan NIM: ");
+        String nim = input.nextLine();
+
+        System.out.print("Masukkan Umur: ");
+        String umur = input.nextLine();
+
+        System.out.print("Masukkan Gender: ");
+        String gender = input.nextLine();
+
+        System.out.print("Masukkan Alamat: ");
+        String alamat = input.nextLine();
+
+        System.out.print("Masukkan Hobi: ");
+        String hobi = input.nextLine();
+
+        System.out.println("\n=== BIODATA ANDA ===");
+        System.out.printf("%s%s%n","Nama\t: ",nama);
+        System.out.printf("%s%s%n","NIM\t: ", nim);
+        System.out.printf("%s%s tahun%n","Umur\t: ", umur);
+        System.out.printf("%s%s%n","Gender\t: ", gender);
+        System.out.printf("%s%s%n","Alamat\t: ", alamat);
+        System.out.printf("%s%s%n","Hobi\t: ", hobi);
+        input.close();
+        
+    }
+}
