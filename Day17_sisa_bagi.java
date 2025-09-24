bb4704a258c774ddac64cfc9adee2b20db873cbd
Paduka-Suryan <arsilid2@gmail.com>
2025-09-24 19:18:49 +0700

    Add files via upload

---
+++ Day17_sisa_bagi.java
@@ -0,0 +1,11 @@
+public class Day17_sisa_bagi {
+    public static void main(String[] args) {
+
+    int a = 11;
+    int b = 3;
+
+    System.out.print("sisa bagi dari 11 dibagi 3 adalah : "+a%b);
+
+    }
+    
+}
