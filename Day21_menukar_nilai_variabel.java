5fdc7048c47fe64e8521e7e3d6da8277a474da4f
Paduka-Suryan <arsilid2@gmail.com>
2025-09-28 20:13:52 +0800

    Update Day21_menukar_nilai_variabel.java

--- Day21_menukar_nilai_variabel.java
+++ Day21_menukar_nilai_variabel.java
@@ -2,28 +2,20 @@ public class Day21_menukar_nilai_variabel {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = 30;
-        int d = 40;
 
         System.out.println("Sebelum ditukar:");
         System.out.println("Nilai a = " + a);
         System.out.println("Nilai b = " + b);
-        System.out.println("Nilai c = " + c);
-        System.out.println("Nilai d = " + d);
 
-        // Menukar nilai menggunakan variabel sementara
-        int tukar = a;
-        a = b;
-        b = c;
-        c = d;
-        d = tukar;
+        a = a+b; // 30
+        b = a-b; // 30-20=10
+        a = a-b; // 30-10=20
         
         System.out.println("Setelah ditukar:");
         System.out.println("Nilai a = " + a);
         System.out.println("Nilai b = " + b);
-        System.out.println("Nilai c = " + c);
-        System.out.println("Nilai d = " + d);
     }
     
 }
+
 
