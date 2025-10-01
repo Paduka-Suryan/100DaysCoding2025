ae142d400be1509094e5144cfda9701dd992ef0b
Paduka-Suryan <arsilid2@gmail.com>
2025-10-01 23:36:56 +0700

    Add files via upload

---
+++ Day25_increment_decrement.java
@@ -0,0 +1,15 @@
+public class Day25_increment_decrement {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Nilai awal a: " + a);
+        
+        // Increment
+        a++;
+        System.out.println("Setelah increment (a++): " + a);
+        
+        // Decrement
+        a--;
+        System.out.println("Setelah decrement (a--): " + a);
+    }
+    
+}
