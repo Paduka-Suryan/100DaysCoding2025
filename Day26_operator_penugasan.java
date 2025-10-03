1d5b7595e5cc18b9767b7cae19819296d74fe641
Paduka-Suryan <arsilid2@gmail.com>
2025-10-03 21:05:45 +0700

    Add files via upload

---
+++ Day26_operator_penugasan.java
@@ -0,0 +1,27 @@
+public class Day26_operator_penugasan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Penugasan sederhana
+        a += b; // a = a + b
+        System.out.println("Hasil a += b: " + a); // Output: 15
+
+        // Penugasan pengurangan
+        a -= b; // a = a - b
+        System.out.println("Hasil a -= b: " + a); // Output: 10
+
+        // Penugasan perkalian
+        a *= b; // a = a * b
+        System.out.println("Hasil a *= b: " + a); // Output: 50
+
+        // Penugasan pembagian
+        a /= b; // a = a / b
+        System.out.println("Hasil a /= b: " + a); // Output: 10
+
+        // Penugasan modulus
+        a %= b; // a = a % b
+        System.out.println("Hasil a %= b: " + a); // Output: 0
+    }
+
+}
