00992e918fb66816e85cd039d03e881f71f01145
Paduka-Suryan <arsilid2@gmail.com>
2025-10-07 18:59:06 +0700

    Add files via upload

---
+++ Day30_operator_perbandingan.java
@@ -0,0 +1,12 @@
+public class Day30_operator_perbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 10;
+
+        System.out.println("a >= b: " + (a >= b)); // true
+        System.out.println("a <= b: " + (a <= b)); // false
+        System.out.println("a <= c: " + (a == c)); // true
+    }
+
+}
