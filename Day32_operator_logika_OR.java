63e0045757f66199844dd6e0b8023fd3697daf69
Paduka-Suryan <arsilid2@gmail.com>
2025-10-09 21:58:22 +0700

    Add files via upload

---
+++ Day32_operator_logika_OR.java
@@ -0,0 +1,16 @@
+public class OperatorOR {
+    public static void main(String[] args) {
+        // Operator OR (||) - hasilnya true jika salah satu true
+        System.out.println("true  || true  = " + (true || true));
+        System.out.println("true  || false = " + (true || false));
+        System.out.println("false || true  = " + (false || true));
+        System.out.println("false || false = " + (false || false));
+        
+        // Contoh sederhana
+        boolean punyaKartu = true;
+        boolean usiaTua = false;
+        boolean dapatDiskon = punyaKartu || usiaTua;
+        
+        System.out.println("\nDapat diskon: " + dapatDiskon);
+    }
+}
