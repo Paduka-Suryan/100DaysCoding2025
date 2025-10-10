387600a55d3c91dbc0de351fd52768cb8840b441
Paduka-Suryan <arsilid2@gmail.com>
2025-10-10 19:44:07 +0700

    Add files via upload

---
+++ Day33_operator_logika_NOT.java
@@ -0,0 +1,12 @@
+public class Day33_operator_logika_NOT {
+    public static void main (String [] args){
+        int a = 10;
+        int b = 5;
+
+        // Menggunakan operator logika NOT (!)
+        System.out.println("!(a > b): " + !(a > b)); // false
+        System.out.println("!(a < b): " + !(a < b)); // true
+
+    }
+
+}
