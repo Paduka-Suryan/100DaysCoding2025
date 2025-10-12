3475b95390ae9361c306d0bb763c815250ff3b2a
Paduka-Suryan <arsilid2@gmail.com>
2025-10-12 19:40:56 +0700

    Add files via upload

---
+++ Day35_percabangan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan nilai : ");
+        int nilai = sc.nextInt();
+        
+        if (nilai >= 70){
+            System.out.print("Selamat, anda lulus");
+            }else {
+                System.out.print("Maaf, anda blum lulus");
+            }
+    }
+}
