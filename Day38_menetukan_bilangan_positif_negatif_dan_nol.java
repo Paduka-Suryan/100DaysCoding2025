2e4f1e38bec03b8bf4bc9ae752d13d82f1425bed
Paduka-Suryan <arsilid2@gmail.com>
2025-10-15 17:40:58 +0700

    Add files via upload

---
+++ Day38_menetukan_bilangan_positif_negatif_dan_nol.java
@@ -0,0 +1,17 @@
+public class Day38_menetukan_bilangan_positif_negatif_dan_nol {
+    public static void main (String [] args){
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    System.out.print("Masukkan sebuah bilangan: ");
+    int number = sc.nextInt();
+        // Menentukan bilangan positif, negatif, atau nol
+        if (number > 0) {
+            System.out.println(number + " adalah bilangan positif");
+        } else if (number < 0) {
+            System.out.println(number + " adalah bilangan negatif");
+        } else {
+            System.out.println("Bilangan yang dimasukkan adalah nol");
+        }
+
+    }
+
+}
