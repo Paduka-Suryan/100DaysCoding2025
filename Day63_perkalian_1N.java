65a7a510e2ccea4a8d4cc48c9c49e387b45d34be
Paduka-Suryan <arsilid2@gmail.com>
2025-11-09 16:12:30 +0700

    Add files via upload

---
+++ Day63_perkalian_1N.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Day58_N1_1N {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+        
+        int total = 1;
+        
+        System.out.println("\n=== PERKALIAN 1 SAMPAI " + N + " ===");
+        // Cetak 1 sampai N
+        for(int i = 1; i <= N; i++) {
+            System.out.print(i);
+            if(i<N){
+                System.out.print(" * ");
+            }
+            total *= i;
+        }
+        System.out.println(" = " + total);
+        input.close();
+    }
+}
