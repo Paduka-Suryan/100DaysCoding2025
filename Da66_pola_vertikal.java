45fd65f5174e6dd31c4051cdd78a877f0f18f8a2
Paduka-Suryan <arsilid2@gmail.com>
2025-11-12 17:51:08 +0700

    Add files via upload

---
+++ Da66_pola_vertikal.java
@@ -0,0 +1,10 @@
+public class Day66_pola_vertikal {
+	public static void main(String[] args) {
+		
+		System.out.println("=== POLA VERTIKAL ===");
+		
+		for (int i = 1 ; i <= 5 ; i++){
+		    System.out.println("*");
+		}
+	}
+}
