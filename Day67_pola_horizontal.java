1289ed229abd1a02b425e2b298ce5a91ab5a3794
Paduka-Suryan <arsilid2@gmail.com>
2025-11-13 19:51:41 +0700

    Add files via upload

---
+++ Day67_pola_horizontal.java
@@ -0,0 +1,11 @@
+public class Da67_pola_horizontal {
+	public static void main(String[] args) {
+		
+		System.out.println(" === POLA HORIZONTAL === \n");
+		
+		for ( int i = 1 ; i <= 8 ; i++ ){
+		    System.out.print("* ");
+		}
+		
+	}
+}
