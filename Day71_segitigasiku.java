0dd52f6e99d99fae6e1d062c2f52a1328d03b39e
Paduka-Suryan <arsilid2@gmail.com>
2025-11-17 20:22:10 +0700

    Add files via upload

---
+++ Day71_segitigasiku.java
@@ -0,0 +1,13 @@
+public class Day71_segitigasiku {
+	public static void main(String[] args) {
+		
+		int tinggi = 8;
+		
+		for (int i = 1 ; i <= tinggi ; i++){
+		    for (int j = 1 ; j <= i ; j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
