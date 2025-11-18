942c4a5172fea38dea03d13acb2aa1a7427405a3
Paduka-Suryan <arsilid2@gmail.com>
2025-11-18 20:08:48 +0700

    Add files via upload

---
+++ Day72_segitigasiku_terbalik.java
@@ -0,0 +1,14 @@
+public class Day72_segitigasiku_terbalik {
+    public static void main(String[] args) {
+        int tinggi = 5;
+        
+        System.out.println("=== SEGITIGA SIKU-SIKU TERBALIK KIRI ATAS ===");
+        
+        for(int i = tinggi; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
