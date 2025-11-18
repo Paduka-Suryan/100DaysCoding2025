0fdc5c5b21ca8e5d40300ec1a084af26d45d83a5
Paduka-Suryan <arsilid2@gmail.com>
2025-11-18 21:10:08 +0800

    Update Day72_segitigasiku_terbalik.java

--- Day72_segitigasiku_terbalik.java
+++ Day72_segitigasiku_terbalik.java
@@ -1,11 +1,11 @@
 public class Day72_segitigasiku_terbalik {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         int tinggi = 5;
         
-        System.out.println("=== SEGITIGA SIKU-SIKU TERBALIK KIRI ATAS ===");
+        System.out.println("=== SEGITIGA SIKU TERBALIK ===");
         
-        for(int i = tinggi; i >= 1; i--) {
-            for(int j = 1; j <= i; j++) {
+        for (int i = tinggi; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
