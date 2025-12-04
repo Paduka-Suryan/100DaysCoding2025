beaad57c5a17b56fadf81ed4e8929e9c0ead77db
Paduka-Suryan <arsilid2@gmail.com>
2025-12-04 19:50:52 +0700

    Add files via upload

---
+++ Day88_MengaksesElemenArray.java
@@ -0,0 +1,9 @@
+public class Day88_MengaksesElemenArray {
+	public static void main(String[] args) {
+		String [] nama = {"PADUKA","ARSYIL","SURYAN","LIKAAA"};
+		System.out.print("Isi Array: ");
+		for (String name : nama){
+		    System.out.print(name+" ");
+		}
+	}
+}
