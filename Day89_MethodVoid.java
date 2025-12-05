628867372aa1cdf345e65b24ef080e02f03fab76
Paduka-Suryan <arsilid2@gmail.com>
2025-12-05 10:50:35 +0700

    Add files via upload

---
+++ Day89_MethodVoid.java
@@ -0,0 +1,13 @@
+public class Day89_MethodVoid {
+    
+    static void sapa(String nama) {
+        System.out.println("Halo "+nama+"!");
+    }
+    
+    public static void main(String[] args) {
+        
+        sapa("Likaaa");
+        sapa("Padukaaa");
+        sapa("Mabar");
+    }
+}
