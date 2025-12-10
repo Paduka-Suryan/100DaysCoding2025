cf9f2f086c2d8fef5cb333d9f5723234032b0ed7
Paduka-Suryan <arsilid2@gmail.com>
2025-12-10 20:32:07 +0700

    Add files via upload

---
+++ Day94_LatihanMethodVoid.java
@@ -0,0 +1,12 @@
+public class Main {
+	
+	static void a (){
+	    String a = "Padukaaa";
+	    String b = "Likaaa";
+	    System.out.println(a+" dan "+b+" sedang tidak mood");
+	}
+	public static void main(String[] args) {
+		a();
+		
+	}
+}
