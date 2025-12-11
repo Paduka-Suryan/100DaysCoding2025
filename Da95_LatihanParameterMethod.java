62dabc66943af2a5f735a61c740a90c83aadce8e
Paduka-Suryan <arsilid2@gmail.com>
2025-12-11 22:13:20 +0700

    Add files via upload

---
+++ Da95_LatihanParameterMethod.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	static void a (String nama){
+	    System.out.print(nama+" dan ");
+	}
+	
+	static void b (String nama2){
+	    System.out.print(nama2+" adalah definisi cinta setara");
+	}
+	
+	public static void main(String[] args) {
+		
+		a("padukaaa");
+		b("likaaa");
+		
+	}
+}
